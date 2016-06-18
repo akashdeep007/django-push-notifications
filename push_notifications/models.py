3267b95f725076c95fe43e85ea8bbf97eaa2c451
Akash Deep Verma <akashdeep.verma@jabong.com>
2016-06-18 21:15:26 +0530

    Fix indentation error

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -15,7 +15,7 @@ class Device(models.Model):
 		help_text=_("Inactive devices will not be sent notifications"))
 	user = models.ForeignKey(SETTINGS["USER_MODEL"], blank=True, null=True)
 	date_created = models.DateTimeField(verbose_name=_("Creation date"), auto_now_add=True, null=True)
-    version = models.CharField(verbose_name="App Version", max_length=255, null=True, blank=True)
+	version = models.CharField(verbose_name="App Version", max_length=255, null=True, blank=True)
 
 	class Meta:
 		abstract = True
